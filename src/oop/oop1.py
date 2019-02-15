8bbf41af3494d2ef08ed1caf082ffa37d4029b49
Wanda <doubleuevans@gmail.com>
2019-02-15 10:33:46 -0600

    added add'l derived classes (Flight, Ground) and thier (derived classes)

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -18,9 +18,31 @@
 #
 # Put a comment noting which class is the base class
 
-class Vehicle:
-    def __init__(self, flight_vehicle, ground_vehicle):
-        self.flight_vehicle = flight_vehicle
-        self.ground_vehicle = ground_vehicle
+class Vehicle:  # Base Class
+    def __init__(self):
+        pass
+
+class FlightVehicle(Vehicle):
+    def __init__(self):
+        pass
 
 
+class Starship(FlightVehicle):
+    def __init__(self):
+        pass
+
+class GroundVehicle(Vehicle):
+    def __init__(self):
+        pass
+        
+class Airplane(FlightVehicle):
+    def __init__(self):
+        pass
+
+class Car(GroundVehicle):
+    def __init__(self):
+        pass
+
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        pass
